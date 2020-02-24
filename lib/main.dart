adc6e32344178c717b1ff45691b7410edc109549
Sean Jackson <seanjackson@138-38-190-170.eduroam.bath.ac.uk>
2020-02-24 16:08:11 +0000

    Reads json to page

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,90 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'dart:async' show Future;
+import 'package:flutter/services.dart' show rootBundle;
 
-void main() => runApp(MyApp());
+class Trip {
+  int id;
+  String name;
+  String destination;
+  int timestamp;
+  int rating;
+  int passenger;
+  int luggage;
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  Trip(
+      {this.id,
+      this.name,
+      this.destination,
+      this.timestamp,
+      this.rating,
+      this.passenger,
+      this.luggage});
+
+  factory Trip.fromJson(Map<String, dynamic> parsedJson) {
+    return Trip(
+        id: parsedJson['id'],
+        name: parsedJson['name'],
+        destination: parsedJson['destination'],
+        timestamp: parsedJson['timestamp'],
+        rating: parsedJson['rating'],
+        passenger: parsedJson['passenger'],
+        luggage: parsedJson['luggage']);
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+Future<String> load() async {
+  return await rootBundle.loadString('assets/trips.json');
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+Future<List<Trip>> loadTrips() async {
+  String jsonString = await load();
+  List<dynamic> jsonArray = json.decode(jsonString);
+  List<Trip> trips = jsonArray.map((el) => new Trip.fromJson(el)).toList();
+  return trips;
+}
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  List<Trip> trips = await loadTrips();
+  runApp(MyApp(trips: trips));
+}
+
+class MyApp extends StatefulWidget {
+  final List<Trip> trips;
+
+  MyApp({this.trips});
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
+    print(widget.trips);
 
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+
+    return new MaterialApp(
+      home: new Scaffold(
+          appBar: new AppBar(
+            title: new Text('Load Json'),
+          ),
+          body: new Container(
+              padding: new EdgeInsets.all(20.0),
+              child: new Row(
+                children: <Widget>[
+                  Text(
+                      "${widget.trips.map((el) => "id ${el.id} name ${el.name}")}")  // make this show the whole record and then load for multiple
+                ],
+              ))),
     );
   }
 }
